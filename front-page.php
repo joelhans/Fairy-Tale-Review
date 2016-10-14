8ea8402c13827eaff7471bf6a36f07bb92401a77
Joel Hans <joel.g.hans@gmail.com>
2016-10-14 08:34:09 -0700

    More work on the front page.

--- front-page.php
+++ front-page.php
@@ -1,6 +1,7 @@
 <!-- FRONT PAGE ANNOUNCEMENT -->
 <section class="container front-page-announcement">
   <?php
+    $do_not_duplicate = array();
     query_posts( array(
       'post_type' => 'post',
       'showposts' => 1
@@ -9,6 +10,7 @@
     get_template_part('templates/content-announcement', get_post_type() != 'post' ? get_post_type() : get_post_format());
     endwhile;
     wp_reset_query();
+    $do_not_duplicate[] = $post->ID;
   ?>
 </section>
 <!-- END FRONT PAGE ANNOUNCEMENT -->
@@ -21,12 +23,12 @@
       'post_type' => 'post',
       'showposts' => 3,
       'ignore_sticky_posts' => 1,
-      'post__not_in' => $sticky
+      'post__not_in' => $do_not_duplicate
     ));
     while (have_posts()) : the_post();
     get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
     endwhile;
-    wp_reset_query();
+    $do_not_duplicate[] = $post->ID;
   ?>
 </section>
 <!-- END FEATURED ARTICLE -->
@@ -51,11 +53,12 @@
     'showposts' => 12,
     'offset' => 2,
     'ignore_sticky_posts' => 1,
-    'post__not_in' => $sticky
+    'post__not_in' => $do_not_duplicate
   ));
   while (have_posts()) : the_post();
   get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
   endwhile;
+  $do_not_duplicate[] = $post->ID;
   wp_reset_query();
 ?>
 </section>
--- templates/content.php
+++ templates/content.php
@@ -1,16 +1,19 @@
 <article <?php post_class(); ?>>
 
-  <a class="entry-image">
+  <div class="entry-image">
     <div class="overlay"></div>
-    <?php if ( has_post_thumbnail() ) {
-  	   the_post_thumbnail('large');
-      } ?>
-  </a>
+    <?php if ( has_post_thumbnail() ) { the_post_thumbnail('large'); } ?>
+  </div>
 
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <a class="entry-link" href="<?php the_permalink(); ?>"></a>
+
+  <div class="entry-title">
+    <h2><?php the_title(); ?></h2>
+  </div>
+
+  <div class="entry-meta">
     <?php get_template_part('templates/entry-meta'); ?>
-  </header>
+  </div>
 
   <div class="entry-summary">
     <?php the_excerpt(); ?>
