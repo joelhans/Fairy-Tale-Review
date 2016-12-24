3c89caadf7e4887248fd38ab8993a99f2951b067
Joel Hans <joel.g.hans@gmail.com>
2016-12-24 09:04:55 -0700

    Getting ready for first deployment.

--- page-about.php
+++ page-about.php
@@ -6,8 +6,12 @@
 
 <section class="container page-about">
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+  <section class="about-tagline">
+    <h1><em>Fairy Tale Review</em> is a literary journal dedicated to publishing new, inclusive, and innovative fairy-tale prose and poetry.</h1>
+  </section>
+
+  <?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+  <?php endwhile; ?>
 
 </section>
