ebda563e0963b6b5c1c29d0d80630e12c3eb86ad
Joel Hans <joel.g.hans@gmail.com>
2017-06-11 07:10:57 -0700

    Fixed newsletter bug.

--- templates/rail_newsletter.php
+++ templates/rail_newsletter.php
@@ -6,7 +6,7 @@
       <div id="mc_embed_signup_scroll">
   	<h2>Subscribe to our newsletter</h2>
     <h4>Get quarterly updates on submission opportunities, <em>Fairy Tale Review</em> events, and our latest publications.</h4>
-    <div class="mc-field-group col__3--4">
+    <div class="mc-field-group">
     	<input type="email" placeholder="Enter email address" value="" name="EMAIL" class="required email" id="mce-EMAIL">
     </div>
   	<div id="mce-responses" class="clear">
@@ -14,7 +14,7 @@
   		<div class="response" id="mce-success-response" style="display:none"></div>
   	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
       <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e9d58a233ee6e7b1e2d17daac_4e9a84060c" tabindex="-1" value=""></div>
-    <div class="mc-subscribe-group col__1--4">
+    <div class="mc-subscribe-group">
       <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
   </form>
